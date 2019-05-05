7231b95dc89e0a20d2fa2cf0078a486cff17a44a
Kevin Grimm <49559740+kevinggrimm@users.noreply.github.com>
2019-05-05 15:48:02 -0400

    Create 01_the_computer_store.sql
    Insert problem set questions

---
+++ 01_the_computer_store.sql
@@ -0,0 +1,58 @@
+-- I. Select the names of all the products in the store.
+
+
+-- II. Select the names and the prices of all the products in the store.
+
+
+-- III. Select the name of the products with a price less than or equal to $200.
+
+
+-- IV. Select all the products with a price between $60 and $120.
+
+
+-- V. Select the name and price in cents (i.e., the price must be multiplied by 100).
+
+
+-- VI. Compute the average price of all the products.
+
+
+-- VII. Compute the average price of all products with manufacturer code equal to 2.
+
+
+-- VIII. Compute the number of products with a price larger than or equal to $180.
+
+
+-- IX. Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
+
+
+-- X. Select all the data from the products, including all the data for each product's manufacturer.
+
+
+-- XI. Select the product name, price, and manufacturer name of all the products.
+
+
+-- XII. Select the average price of each manufacturer's products, showing only the manufacturer's code.
+
+
+-- XIII. Select the average price of each manufacturer's products, showing the manufacturer's name.
+
+
+-- XIV. Select the names of manufacturer whose products have an average price larger than or equal to $150.
+
+
+-- XV. Select the name and price of the cheapest product.
+
+
+-- XVI. Select the name of each manufacturer along with the name and price of its most expensive product.
+
+
+-- XVII. Add a new product: Loudspeakers, $70, manufacturer 2.
+
+
+-- XVIII. Update the name of product 8 to "Laser Printer".
+
+
+-- XIX. Apply a 10% discount to all products.
+
+
+-- XX. Apply a 10% discount to all products with a price larger than or equal to $120.
