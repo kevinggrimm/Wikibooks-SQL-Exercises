374176edf9bfbecf93d68efd88c3aa37c13ebec5
Kevin Grimm <49559740+kevinggrimm@users.noreply.github.com>
2019-05-05 15:55:45 -0400

    Create 04_movie_theatres.sql

---
+++ 04_movie_theatres.sql
@@ -0,0 +1,38 @@
+-- 1. Select the title of all movies.
+
+
+
+-- 2. Show all the distinct ratings in the database.
+
+
+
+-- 3. Show all unrated movies.
+
+
+
+-- 4. Select all movie theaters that are not currently showing a movie.
+
+
+
+-- 5. Select all data from all movie theaters and, additionally, the data from the movie that is being shown in the theater (if one is being shown).
+
+
+
+-- 6. Select all data from all movies and, if that movie is being shown in a theater, show the data from the theater.
+
+
+
+-- 7. Show the titles of movies not currently being shown in any theaters.
+
+
+
+-- 8. Add the unrated movie "One, Two, Three".
+
+
+
+-- 9. Set the rating of all unrated movies to "G".
+
+
+
+-- 10. Remove movie theaters projecting movies rated "NC-17".
+
