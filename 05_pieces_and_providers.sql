a09e60127445c5dcea0acd3bb88a351c59c7be28
Kevin Grimm <49559740+kevinggrimm@users.noreply.github.com>
2019-05-05 15:57:09 -0400

    Create 05_pieces_and_providers.sql

---
+++ 05_pieces_and_providers.sql
@@ -0,0 +1,38 @@
+-- 1. Select the name of all the pieces. (Seleccione el nombre de todas las piezas).
+
+
+
+-- 2. Select all the providers' data. (Seleccione todos los datos de los proveedores (providers)).
+
+
+
+-- 3. Obtain the average price of each piece (show only the piece code and the average price).
+
+
+
+-- 4. Obtain the names of all providers who supply piece 1.
+
+
+
+-- 5. Select the name of pieces provided by provider with code "HAL".
+
+
+
+-- 6. For each piece, find the most expensive offering of that piece and include the piece name, provider name, and price (note that there could be two providers who supply the same piece at the most expensive price).
+
+
+
+-- 7. Add an entry to the database to indicate that "Skellington Supplies" (code "TNBC") will provide sprockets (code "1") for 7 cents each.
+
+
+
+-- 8. Increase all prices by one cent.
+
+
+
+-- 9. Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply bolts (code 4).
+
+
+
+-- 10. Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply any pieces (the provider should still remain in the database).
+
